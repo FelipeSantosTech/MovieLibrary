1eb552d1c7d3fccb76f7230c33b85fa82d73a926
FelipeSantosTech <felpsantos.c@gmail.com>
2026-02-18 15:14:29 -0300

    Initial Commit

---
+++ MovieLibrary/MovieLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieLibraryApp.swift
+//  MovieLibrary
+//
+//  Created by Paulo Marcelo Santos on 18/02/26.
+//
+
+import SwiftUI
+
+@main
+struct MovieLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
