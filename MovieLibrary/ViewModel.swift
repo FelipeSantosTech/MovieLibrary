f8d46eeff2e08208422a10b542b1e75fdc721be0
FelipeSantosTech <felpsantos.c@gmail.com>
2026-02-26 10:28:37 -0300

    created view model file with fetch status

---
+++ MovieLibrary/ViewModel.swift
@@ -0,0 +1,33 @@
+//
+//  ViewModel.swift
+//  MovieLibrary
+//
+//  Created by Paulo Marcelo Santos on 26/02/26.
+//
+
+import Foundation
+
+@Observable
+class ViewModel {
+    enum FetchStatus {
+        case notStarted
+        case fetching
+        case success
+        case failed (underlyingError: Error)
+    }
+    private(set) var homeStatus: FetchStatus = .notStarted
+    private let dataFetcher = DataFetcher()
+    var trendingMovies: [Title] = []
+    
+    func getTitles() async {
+        homeStatus = .fetching
+        
+        do {
+            trendingMovies = try await dataFetcher.fetchTitles(for: "movie")
+            homeStatus = .success
+        } catch {
+            print(error)
+            homeStatus = .failed(underlyingError: error)
+        }
+    }
+}
